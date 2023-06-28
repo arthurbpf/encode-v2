0173a8dfebaee7869523667dcc21ce034cee0220
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-06-28 14:49:59 -0300

    style: add prettier

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -1,33 +1,32 @@
-import * as React from "react";
-
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
+import * as React from 'react';
 
 export interface HeadingProps
-  extends React.InputHTMLAttributes<HTMLHeadingElement> {
-  as: "h1" | "h2" | "h3" | "h4";
+	extends React.InputHTMLAttributes<HTMLHeadingElement> {
+	as: 'h1' | 'h2' | 'h3' | 'h4';
 }
 
 const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
-  ({ className, ...props }, ref) => {
-    const Component = props.as;
+	({ className, ...props }, ref) => {
+		const Component = props.as;
 
-    const styles = {
-      h1: "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
-      h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0",
-      h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
-      h4: "scroll-m-20 text-xl font-semibold tracking-tight",
-    };
+		const styles = {
+			h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+			h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0',
+			h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
+			h4: 'scroll-m-20 text-xl font-semibold tracking-tight'
+		};
 
-    return (
-      <Component
-        className={cn(styles[props.as], className)}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
+		return (
+			<Component
+				className={cn(styles[props.as], className)}
+				ref={ref}
+				{...props}
+			/>
+		);
+	}
 );
 
-Heading.displayName = "Heading";
+Heading.displayName = 'Heading';
 
 export { Heading };
