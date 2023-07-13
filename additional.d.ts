af8132467c8a800b7a50a0b38d5cd10b8b0fb19b
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-07-12 22:30:59 -0300

    feat: add alchemy api key to typing system

--- additional.d.ts
+++ additional.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
 	export interface ProcessEnv {
 		readonly NEXT_PUBLIC_CONTRACT_ADDRESS: string;
+		readonly ALCHEMY_API_KEY: string;
 
 		readonly NEXT_PUBLIC_IPFS_API_URL: string;
 		readonly PINATA_API_KEY: string;
