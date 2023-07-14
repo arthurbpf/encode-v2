92e25ef8cf644cff9b4ad95fefd5c888b1c02671
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-07-13 21:32:34 -0300

    feat: put icons before text on homepage

--- src/app/action-buttons.tsx
+++ src/app/action-buttons.tsx
@@ -13,24 +13,24 @@ const IndexPageActionButtons = () => {
 		<div className="flex flex-col gap-2">
 			{!userAddress && (
 				<Button className="flex w-full gap-2" onClick={connectWallet}>
-					Connect your wallet
 					<LuWallet />
+					Connect your wallet
 				</Button>
 			)}
 
 			{!!userAddress && (
 				<Link href="/mint">
 					<Button className="flex w-full gap-2">
-						Start minting
 						<LuHammer />
+						Start minting
 					</Button>
 				</Link>
 			)}
 
 			<Link href="/search">
 				<Button className="flex w-full gap-2">
-					Check out community created tokens
 					<LuSearch />
+					Check out community created tokens
 				</Button>
 			</Link>
 		</div>
