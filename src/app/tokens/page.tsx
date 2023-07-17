3a893379f3ef774861b09d141cb3c1ee3ca22a50
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-07-17 19:25:36 -0300

    feat: implement token listing

--- src/app/tokens/page.tsx
+++ src/app/tokens/page.tsx
@@ -1,5 +1,19 @@
-import { getEncodeAlchemyContract } from '@/lib/ethers/utils';
+import TokenMetadataCard from '@/components/token-metadata-card';
+import { Heading } from '@/components/ui/heading';
+import { listTokens } from '@/lib/ethers/utils';
 
-export default function TokensPage() {
-	return <div></div>;
+export default async function ListTokensPage() {
+	const tokens = await listTokens();
+
+	return (
+		<div className="p-10 flex flex-col gap-8 flex-1">
+			<Heading as="h1">Tokens created by the community!</Heading>
+
+			<div>
+				{tokens.map((token) => (
+					<TokenMetadataCard key={token.id} token={token} />
+				))}
+			</div>
+		</div>
+	);
 }
---
+++ src/components/token-metadata-card.tsx
@@ -0,0 +1,35 @@
+import { TokenInfo } from '@/lib/ethers/types';
+import { formatDistance } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { LuClock } from 'react-icons/lu';
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle
+} from './ui/card';
+
+export default function TokenMetadataCard({ token }: { token: TokenInfo }) {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle className="flex justify-between">
+					<div>
+						# {token.id} - {token.metadata.title}
+					</div>
+				</CardTitle>
+				<CardDescription>{token.metadata.description}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>
+					<span className="flex items-center justify-center gap-2 leading-7 [&:not(:first-child)]:mt-2">
+						<LuClock />{' '}
+						{formatDistance(token.creationDate, new Date(), { locale: ptBR })}
+					</span>
+				</p>
+			</CardContent>
+		</Card>
+	);
+}
