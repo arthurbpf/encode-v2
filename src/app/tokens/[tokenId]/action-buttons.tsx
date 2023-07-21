7c21c10569081bf230069cc1cf04ce8fd31cf592
Arthur Fernandes <arthurbpfernandes@gmail.com>
2023-07-20 21:40:00 -0300

    feat: start implementation of dialogs and sheets

--- src/app/tokens/[tokenId]/action-buttons.tsx
+++ src/app/tokens/[tokenId]/action-buttons.tsx
@@ -1,10 +1,118 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from '@/components/ui/dialog';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger
+} from '@/components/ui/sheet';
 import { TokenInfo } from '@/lib/ethers/types';
 import { trimAddress } from '@/lib/ethers/utils';
 import { useEthersStore } from '@/stores/ethers';
+import { useState } from 'react';
 import { LuDollarSign, LuList, LuMegaphone, LuTrash } from 'react-icons/lu';
+
+function SellTokenDialog() {
+	const [dialogOpen, setDialogOpen] = useState(false);
+
+	return (
+		<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+			<DialogTrigger asChild>
+				<Button variant={'confirmation'} className="flex gap-2">
+					<LuMegaphone />
+					List token for sale
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>{'List token for sale'}</DialogTitle>
+					<DialogDescription>
+						{
+							'This will make your token available for immediate purchase for the value specified.'
+						}
+					</DialogDescription>
+				</DialogHeader>
+
+				<DialogFooter>
+					<Button variant={'secondary'} onClick={() => setDialogOpen(false)}>
+						{'Cancel'}
+					</Button>
+					<Button onClick={() => setDialogOpen(false)}>{'Post listing'}</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+function CancelSellingDialog() {
+	const [dialogOpen, setDialogOpen] = useState(false);
+
+	return (
+		<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+			<DialogTrigger asChild>
+				<Button variant={'destructive'} className="flex gap-2">
+					<LuTrash />
+					Delete selling listing
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]"></DialogContent>
+		</Dialog>
+	);
+}
+
+function BidDialog() {
+	const [dialogOpen, setDialogOpen] = useState(false);
+
+	return (
+		<Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+			<DialogTrigger asChild>
+				<Button variant={'confirmation'} className="flex gap-2">
+					<LuDollarSign />
+					Make a bid!
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]"></DialogContent>
+		</Dialog>
+	);
+}
+
+function ListBidSheet() {
+	return (
+		<Sheet>
+			<SheetTrigger asChild>
+				<Button className="flex gap-2">
+					<LuList />
+					View buying bids
+				</Button>
+			</SheetTrigger>
+			<SheetContent side="right" className="flex flex-col">
+				<SheetHeader>
+					<SheetTitle>Bids for this token</SheetTitle>
+					<SheetDescription>
+						These are the bids that have been made for this token.
+					</SheetDescription>
+				</SheetHeader>
+				<ScrollArea>
+					<div className="grid p-6"></div>
+					<ScrollBar />
+				</ScrollArea>
+			</SheetContent>
+		</Sheet>
+	);
+}
 
 export default function TokenDetailsActionButtons({
 	token
@@ -16,31 +124,15 @@ export default function TokenDetailsActionButtons({
 
 	return (
 		<div className="flex gap-2">
-			{token.owner === userAddress && (
-				<Button variant={'confirmation'} className="flex gap-2">
-					<LuMegaphone />
-					List token for sale
-				</Button>
-			)}
+			{token.owner === userAddress && <SellTokenDialog />}
 
 			{token.owner === userAddress && token.sellingListing.price !== 0n && (
-				<Button variant={'destructive'} className="flex gap-2">
-					<LuTrash />
-					Delete selling listing
-				</Button>
+				<CancelSellingDialog />
 			)}
 
-			{token.owner !== userAddress && (
-				<Button variant={'confirmation'} className="flex gap-2">
-					<LuDollarSign />
-					Make a bid!
-				</Button>
-			)}
+			{token.owner !== userAddress && <BidDialog />}
 
-			<Button className="flex gap-2">
-				<LuList />
-				View buying bids
-			</Button>
+			<ListBidSheet />
 		</div>
 	);
 }
